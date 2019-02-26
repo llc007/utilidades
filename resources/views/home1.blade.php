e367a632e5148472aa23540f819968d6655ac467
llc007 <llc007.1@gmail.com>
2019-02-26 14:56:16 -0300

    Modulo de calendario y correo

---
+++ resources/views/home1.blade.php
@@ -0,0 +1,118 @@
+@extends('layout.master')
+<style>
+    .borde {
+        border: 2px solid darkolivegreen;
+    }
+
+    #contenedorTarjetas {
+        margin-top: 5%;
+        display: flex;
+        justify-content: space-between;
+        width: 90%;
+    }
+</style>
+
+@section('content')
+    <div class='tiles'>
+
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        {{--CALENDARIO--}}
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="/calendario2"></a>
+            <a href="/calendario2"></a>
+            <a href="/calendario2"></a>
+            <a href="/calendario2"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+        <div class='col'>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+            <div class='box'></div>
+        </div>
+    </div>
+@endsection
